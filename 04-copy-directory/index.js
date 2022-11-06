eea83da29c06b3e8d2bbe17bc9031afcececa4d6
Peter Terentev <petrfalkovsky@yandex.ru>
2022-11-06 13:20:38 +0300

    feat(04-copy-directory): done fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fsProm = require('fs/promises');
+const path = require('path');
+const sourceFolderPath = path.join(__dirname, 'files');
+const newFolderPath = path.join(__dirname, 'files-copy');
 
+async function copyDir() {
+    const deleteIfThere = await fsProm.rm(newFolderPath, {recursive: true, force: true});
+    const newFolder = await fsProm.mkdir(newFolderPath,  { recursive: true });
+    const whatInsideSourceFolder = await fsProm.readdir(sourceFolderPath);
+
+    whatInsideSourceFolder.forEach((file) => {
+    fsProm.copyFile(path.join(sourceFolderPath, file), path.join(newFolderPath, file));
+  })
+
+};
+copyDir();
